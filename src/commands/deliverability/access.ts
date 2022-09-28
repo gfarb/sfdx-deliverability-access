da14d30eb30f9b071eedc422a982ab61d4e15365
gfarb <gfarb@github.com>
2022-09-28 18:53:06 -0400

    Puppeteer implementation

--- src/commands/deliverability/access.ts
+++ src/commands/deliverability/access.ts
@@ -3,10 +3,7 @@ import * as util from 'util';
 import { exec } from 'child_process';
 import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages, SfdxError } from '@salesforce/core';
-import webdriver = require('selenium-webdriver');
-import chrome = require('selenium-webdriver/chrome');
-import until = require('selenium-webdriver/lib/until');
-import chromedriver = require('chromedriver');
+import puppeteer = require('puppeteer');
 const execPromise = util.promisify(exec);
 
 const accessValuesMap = new Map<string, string>([
@@ -56,10 +53,14 @@ export default class Access extends SfdxCommand {
   public async run(): Promise<void> {
     this.ux.startSpinner('Attempting to set Email Deliverability Access Level');
     const accessLevel = this.flags.level as string;
-    const childElementIndex = String(accessValuesMap.get(accessLevel?.toLowerCase()));
-    if (childElementIndex !== 'undefined' && Number(childElementIndex) > -1 && Number(childElementIndex) < 3) {
+    const accessLevelValue = String(accessValuesMap.get(accessLevel?.toLowerCase()));
+    if (accessLevelValue !== 'undefined' && Number(accessLevelValue) > -1 && Number(accessLevelValue) < 3) {
       const accessUrl: string = await this.parseOrgData(String(this.flags.user));
-      void this.toggleDeliverability(accessUrl, childElementIndex);
+      try {
+        void this.toggleDeliverability(accessUrl, accessLevelValue);
+      } catch (error) {
+        this.ux.stopSpinner(`❌ Access Level could not be set. The following error occurred:\n${String(error)}`);
+      }
     } else {
       this.ux.stopSpinner('❌ Access Level could not be set.');
       throw new SfdxError(`${accessLevel} is not a valid value for the Level flag.`, 'Invalid value for Level flag');
@@ -80,50 +81,42 @@ export default class Access extends SfdxCommand {
       );
     } catch (error) {
       this.ux.stopSpinner('❌ Access Level could not be set.');
-      const errorMessage = `Unable to parse url from 'sfdx force:org:open' command. Please make sure you have a default org set or you are passing a valid username/alias with the '-u' or '--user' flag.\n\n${String(
-        error
-      )}`;
-      throw new SfdxError(errorMessage);
+      throw new SfdxError(
+        `Unable to parse url from 'sfdx force:org:open' command. Please make sure you have a default org set or you are passing a valid username/alias with the '-u' or '--user' flag.\n\n${String(
+          error
+        )}`
+      );
     }
   }
 
-  private async toggleDeliverability(accessUrl: string, childElementIndex: string): Promise<void> {
-    let driver;
+  private async toggleDeliverability(accessUrl: string, accessLevelValue: string): Promise<void> {
+    let browser;
+    let error;
     try {
-      this.ux.setSpinnerStatus('Opening headless browser session to toggle deliverability access level.');
-      const service = new chrome.ServiceBuilder(String(chromedriver.path)).build();
-      chrome.setDefaultService(service);
-      const options = new chrome.Options()
-        .addArguments('--no-sandbox')
-        .addArguments('--disable-dev-shm-usage')
-        .addArguments('--headless');
-      driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
-    } catch (error) {
-      if (String(error).includes('version of ChromeDriver only supports Chrome version')) {
-        this.ux.stopSpinner('❌ Access Level could not be set.');
-        this.ux.log(`Chromedriver path: ${chromedriver.path}`);
-        throw new SfdxError(String(error), ' Please make sure you have the correct Chromedriver version installed');
-      }
-    }
-    if (driver === undefined) return;
-    try {
-      await driver.manage().setTimeouts({ implicit: 30000, pageLoad: 30000, script: 30000 });
-      await driver.get(accessUrl);
-      this.ux.setSpinnerStatus('Setting deliverability access level.');
-      await driver.wait(until.titleIs('Deliverability | Salesforce'), 30000);
-      await driver.sleep(3000);
-      await driver.switchTo().frame(0);
-      await driver.executeScript(
-        `document.getElementsByClassName('pbSubsection')[0].children[0].children[0].children[0].children[0].children[0].children[${childElementIndex}].selected=true`
+      browser = await puppeteer.launch({
+        headless: true,
+        args: ['--disable-web-security', '--no-sandbox'],
+      });
+      const page = await browser.newPage();
+      await page.goto(accessUrl);
+      const frame = await page.waitForSelector(
+        '#setupComponent > div.setupcontent > div > div > force-aloha-page > div > iframe'
       );
-      await driver.findElement(webdriver.By.id('thePage:theForm:editBlock:buttons:saveBtn')).click();
-      await driver.sleep(1000);
+      const contentFrame = await frame?.contentFrame();
+      const accessLevelField = await contentFrame?.waitForSelector(
+        '#thePage\\:theForm\\:editBlock\\:sendEmailAccessControlSection\\:sendEmailAccessControl\\:sendEmailAccessControlSelect'
+      );
+      await accessLevelField?.select(accessLevelValue);
+      await contentFrame?.click('#thePage\\:theForm\\:editBlock\\:buttons\\:saveBtn');
+      await contentFrame?.waitForSelector('#thePage\\:theForm\\:successText');
+      await browser.close();
       this.ux.stopSpinner('🎉 Email Deliverability Access Level has been set!');
-    } catch (error) {
-      this.ux.stopSpinner('❌ Access Level could not be set.');
-      this.ux.log(`The following error occurred:\n${String(error)}`);
+    } catch (err) {
+      error = String(err);
     } finally {
-      await driver.quit();
+      if (browser !== undefined) await browser.close();
+      if (error !== undefined)
+        this.ux.stopSpinner(`❌ Access Level could not be set. The following error occurred:\n${String(error)}`);
     }
   }
 }
